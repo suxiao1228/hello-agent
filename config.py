110a97fab8ae80f384923a9e1a3b7f312472708e
suxiao6 <suxiao6@noreply.gitcode.com>
2026-04-21 22:46:41 +0800

    hello-agent1

--- config.py
+++ config.py
@@ -6,4 +6,4 @@
 API_KEY = "sk-SaATBPqtUSFwqRNF27F090B83e6545Da819cBbB2233631Ec"
 BASE_URL = "https://aihubmix.com/v1"
 MODEL_ID = "coding-glm-5.1-free"
-TAVILY_API_KEY = ""
+TAVILY_API_KEY = "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"
